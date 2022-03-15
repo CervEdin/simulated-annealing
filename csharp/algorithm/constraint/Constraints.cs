f0ba6902f48b63c3bc485c8fb98f4dbe5120ec00
Erik Cervin Edin <erik@cervined.in>
2022-03-15 17:48:33 +0100

    doc:	Add documentation to Constraints

--- csharp/algorithm/constraint/Constraints.cs
+++ csharp/algorithm/constraint/Constraints.cs
@@ -5,10 +5,23 @@ namespace algorithm.constraint
 {
     public static class Constraints
     {
+        /// <summary>
+        /// The AllDifferent constraint is true IFF
+        /// all values in the list are different.
+        /// </summary>
+        /// <param name="ints"></param>
+        /// <returns></returns>
         public static bool AllDifferent(
             this ICollection<int> ints
         ) => ints.Count == ints.ToHashSet().Count;
 
+        /// <summary>
+        /// The Circuit constraint is true IFF
+        /// the list represents a hamiltonian circuit.
+        /// (implies AllDifferent)
+        /// </summary>
+        /// <param name="ints"></param>
+        /// <returns></returns>
         public static bool Circuit(
             this ICollection<int> ints
         ) => ints

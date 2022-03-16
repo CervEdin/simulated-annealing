7fe2219e4089237659d080cc2657757e78fccecc
Erik Cervin Edin <erik@cervined.in>
2022-03-16 15:21:42 +0100

    algo/constraint:	Move route and circuit from .solver to .constraint

--- csharp/algorithm/constraint/Circuit.cs
+++ csharp/algorithm/constraint/Circuit.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using algorithm.solver;
 
 namespace algorithm.constraint
 {
--- csharp/algorithm/constraint/Route.cs
+++ csharp/algorithm/constraint/Route.cs
@@ -1,9 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using algorithm.constraint;
 
-namespace algorithm.solver
+namespace algorithm.constraint
 {
     public class Route
     {
